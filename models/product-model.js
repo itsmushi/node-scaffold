2b1e5bc276c2b7ad6802b2c433a52cba3af7f0dd
itsmushi <jmushi175@gmail.com>
2022-02-14 01:29:52 +0300

    added relationship

--- models/product-model.js
+++ models/product-model.js
@@ -1,8 +1,22 @@
 const { Model }=require('objection')
+const CustomerModel=require('./customer-model')
 
 
+//we assume customer has only one product
+//one product can belong to none or more than one customer
 class Product extends Model{
     static tableName="products"
+
+    static relationMappings={
+        owner:{
+            relation:Model.HasManyRelation,
+            modelClass:CustomerModel
+        },
+        join:{
+            from:'products.id',
+            to:'customer.product_id'
+        }
+    }
 }   
 
 module.exports=Product
